439745abde9e1f031f94bf6799b17aa5546b2a0a
Natthaphon kanththam <nptkkp.1048@gmail.com>
2024-09-07 17:44:25 +0700

    update next-config to ignore eslint during build-time

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-  // ตรวจสอบ TypeScript files
+  // Check TypeScript files
   '**/*.(ts|tsx)': () => 'npx tsc --noEmit',
 
-  // Lint และฟอร์แมต TypeScript และ JavaScript
+  // Lint and Format TypeScript and JavaScript
   '**/*.(ts|tsx|js)': (filenames) => [
     `npx eslint --fix ${filenames.join(' ')}`,
     `npx prettier --write ${filenames.join(' ')}`,
   ],
 
-  // ฟอร์แมต MarkDown และ JSON
+  // Format MarkDown and JSON
   '**/*.(md|json)': (filenames) => `npx prettier --write ${filenames.join(' ')}`,
 };
